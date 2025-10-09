3980b7d2b4e548a8bb2f57e1abd42df0d168dcf8
cslabiano <cslabiano@up.edu.ph>
2025-10-09 13:54:18 +0800

    feat: initialize screens

---
+++ safebite/lib/navbar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import 'screens/dashboard.dart';
+import 'screens/camera.dart';
+import 'screens/profile.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({super.key});
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: IndexedStack(
+          index: _selectedIndex,
+          children: const <Widget>[Dashboard(), Camera(), Profile()],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: _selectedIndex == 0
+                ? const Icon(Icons.home)
+                : const Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: _selectedIndex == 1
+                ? const Icon(Icons.inventory)
+                : const Icon(Icons.inventory_outlined),
+            label: 'Donations',
+          ),
+          BottomNavigationBarItem(
+            icon: _selectedIndex == 2
+                ? const Icon(Icons.person)
+                : const Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          setState(
+            () {
+              _selectedIndex = index;
+            },
+          );
+        },
+      ),
+    );
+  }
+}
