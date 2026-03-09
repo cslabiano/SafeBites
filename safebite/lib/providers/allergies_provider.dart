b3bbc2b5f756db9a60d07c2617ff2a1a18fe3f5a
cslabiano <cslabiano@up.edu.ph>
2026-03-09 13:17:12 +0800

    feat: add allergies provider

---
+++ safebite/lib/providers/allergies_provider.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AllergiesProvider with ChangeNotifier {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  List<String> _allergies = [];
+
+  List<String> get allergies => _allergies;
+
+  /// fetch user allergies
+  Future<void> fetchAllergies() async {
+    final user = _auth.currentUser;
+    if (user == null) return;
+
+    final doc = await _firestore.collection("users").doc(user.uid).get();
+
+    if (doc.exists) {
+      final data = doc.data();
+
+      _allergies = List<String>.from(data?["allergies"] ?? []);
+      notifyListeners();
+    }
+  }
+
+  /// add allergy
+  Future<void> addAllergy(String allergen) async {
+    final user = _auth.currentUser;
+    if (user == null) return;
+
+    await _firestore.collection("users").doc(user.uid).update({
+      "allergies": FieldValue.arrayUnion([allergen])
+    });
+
+    _allergies.add(allergen);
+    notifyListeners();
+  }
+
+  /// delete allergy
+  Future<void> removeAllergy(String allergen) async {
+    final user = _auth.currentUser;
+    if (user == null) return;
+
+    await _firestore.collection("users").doc(user.uid).update({
+      "allergies": FieldValue.arrayRemove([allergen])
+    });
+
+    _allergies.remove(allergen);
+    notifyListeners();
+  }
+}
